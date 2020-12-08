e709893cbade575e9014e53708eab517ec6a65ef
Wesley Souza <wesley.massine@gmail.com>
2020-12-08 19:27:42 -0300

    remove tests

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 echo 'Running unit tests'
-ng test
+#ng test
 
 echo 'Karma v1.7.1 server started at http://159.89.92.227:9876/'
 
-echo 'Running end-to-end tests'
-ng e2e
+# echo 'Running end-to-end tests'
+# ng e2e
 
 echo 'Project is running at http://localhost:4200/'
