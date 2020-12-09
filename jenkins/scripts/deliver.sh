4f3b306c5fdef84d8a3c4d87b26c78ee1682a876
Bruno Belo <brunohbelo@hotmail.com>
2020-12-09 00:38:31 -0300

    test2

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -15,5 +15,5 @@ echo 'Run your Angular application'
 npm run-scrip start --port 4200
 
 echo 'Now...'
-echo 'Visit http://159.89.92.227:8088 or :4200 to see your application in action.'
+echo 'Visit http://159.89.92.227:4200 or :4200 to see your application in action.'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
