a576ec20ea501052601632af23529f3c0f7df071
Bruno Belo <brunohbelo@hotmail.com>
2020-12-08 23:46:08 -0300

    bugs comentados

--- src/app/soma/somaService.ts
+++ src/app/soma/somaService.ts
@@ -2,8 +2,15 @@ export class SomaService {
 
     public static somar(numero1: number, numero2: number): number {
         console.log(numero1 + numero2);
+
+        // erro test unitário
         // const resultado = Math.ceil( numero1 + numero2);
-        const resultado = Math.ceil( numero1 + numero2);
+
+        // Bug sonar
+        // let i = 0;
+        // i++;
+
+        const resultado = numero1 + numero2;
         return resultado;
     }
 }
