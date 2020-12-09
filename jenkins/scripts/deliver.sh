6fd28268ff400789c72fcf0f1d966932b08cb2d5
Wesley Souza <wesley.massine@gmail.com>
2020-12-08 22:29:37 -0300

    test deliver

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env sh
 
 echo 'Builds your Angular application'
-set -x
+#set -x
 ng build
-set +x
+#set +x
 
 echo 'Run your Angular application'
-echo 'the file ".pidfile".'
-set -x
-ng serve &
-sleep 1
-echo $! > .pidfile
-set +x
+# echo 'the file ".pidfile".'
+# set -x
+# ng serve &
+# sleep 1
+# echo $! > .pidfile
+# set +x
+ng serve --host 159.89.92.227 --port 8088
 
 echo 'Now...'
-echo 'Visit http://159.89.92.227:4200 to see your application in action.'
+echo 'Visit http://159.89.92.227:8088 to see your application in action.'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
