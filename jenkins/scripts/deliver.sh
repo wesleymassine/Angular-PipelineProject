093e5b3a5075737327863ed5f921fa00a2d1e49a
Bruno Belo <brunohbelo@hotmail.com>
2020-12-09 00:42:11 -0300

    ajuste script

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -12,7 +12,7 @@ echo 'Run your Angular application'
 # sleep 1
 # echo $! > .pidfile
 # set +x
-npm run-scrip start --port 4200
+npm run-scrip start
 
 echo 'Now...'
 echo 'Visit http://159.89.92.227:4200 or :4200 to see your application in action.'
