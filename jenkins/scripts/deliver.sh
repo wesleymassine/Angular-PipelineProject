682281dcdcd005e19354033d602e1ceab454d3a5
Wesley Souza <wesley.massine@gmail.com>
2020-12-09 15:11:39 -0300

    Ajustes no pipeline

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env sh
 
 echo 'Builds your Angular application'
-#set -x
+set -x
 npm run-scrip build
-#set +x
+set +x
 
 echo 'Run your Angular application'
-# echo 'the file ".pidfile".'
-# set -x
-# ng serve &
-# sleep 1
-# echo $! > .pidfile
-# set +x
-npm run-scrip start
+echo 'the file ".pidfile".'
+set -x
+npm run-scrip start &
+sleep 1
+echo $! > .pidfile
+set +x
 
 echo 'Now...'
-echo 'Visit http://159.89.92.227:4200 or :4200 to see your application in action.'
+echo 'Visit http://159.89.92.227:4200 to see your application in action.'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
